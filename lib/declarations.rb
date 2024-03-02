153bb63b9381e1011f2de7d5ffe54d9db6e844a2
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-03-02 20:29:24 +0100

    mkdir, git_clone, github_clone declarations

--- lib/declarations.rb
+++ lib/declarations.rb
@@ -267,3 +267,34 @@ def symlink(target, link_name)
     end
   end
 end
+
+# on prepare make sure the directory exists
+def mkdir(path)
+  @mkdir ||= Set.new
+  @mkdir << path
+
+  on_prepare do
+    @mkdir.each do |path|
+      FileUtils.mkdir_p File.expand_path(path)
+    end
+  end
+end
+
+# on prepare make sure a git repository is cloned to directory
+def git_clone(from:, to: nil)
+  @git_clone ||= Set.new
+  @git_clone << {from: from, to: to}
+
+  on_install do
+    @git_clone.each do |item|
+      from = item[:from]
+      to = item[:to]
+      system "git clone #{from} #{to}" unless File.exists?(File.expand_path(to))
+    end
+  end
+end
+
+# git clone for github repositories
+def github_clone(from:, to: nil)
+  git_clone(from: "https://github.com/#{from}", to: to)
+end
