047742ca640e40a0df700244d15854409ceed63c
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-03-06 09:47:38 +0100

    add autologin to user function

--- lib/declarations/user.rb
+++ lib/declarations/user.rb
@@ -5,13 +5,14 @@ require 'etc'
 
 # create a user and assign a set of group. if block is passes the block will run
 # in as this user. block will run during the configure step
-def user(name, groups: [], &block)
+def user(name, groups: [], autologin: nil, &block)
   name = name.to_s
 
   @user ||= {}
   @user[name] ||= {}
   @user[name][:groups] ||= []
   @user[name][:groups] += groups.map(&:to_s)
+  @user[name][:autologin] = autologin unless autologin.nil?
   @user[name][:state] = State.new
   @user[name][:state].apply(block) if block_given?
 
@@ -20,6 +21,15 @@ def user(name, groups: [], &block)
       exists = Etc.getpwnam name rescue nil
       sudo "useradd #{name}" unless exists
       sudo "usermod --groups #{groups.join(",")} #{name}" if groups.any?
+
+      if conf[:autologin]
+        FileUtils.mkdir_p '/etc/systemd/system/getty@tty1.service.d'
+        file '/etc/systemd/system/getty@tty1.service.d/autologin.conf', <<~FILE
+          [Service]
+          ExecStart=
+          ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin #{name} %I $TERM
+        FILE
+      end
 
       fork do
         currentuser = Etc.getpwnam(name)
